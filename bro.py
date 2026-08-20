7a3eeb23ae201964acd414576fc92b8602d44549
accdisfree12-maker <accdisfree12@gmail.com>
2026-08-20 03:41:53 +0100

    Update bro.py

--- bro.py
+++ bro.py
@@ -50,10 +50,10 @@ async def broadcast(
         return
 
     if not interaction.user.guild_permissions.administrator:
-        await interaction.response.send_message("❌ WHERE IS PERIMISSION ? (Administrator) DONT DO THAT AGAIN", ephemeral=True)
+        await interaction.response.send_message("**❌ WHERE IS PERIMISSION ?** (Administrator) **DONT DO THAT AGAIN**", ephemeral=True)
         return
 
-    await interaction.response.send_message(f"🚀 BROADCAST STARTED JUST WAIT WE WILL SEND TO  {interaction.guild.member_count} members...", ephemeral=True)
+    await interaction.response.send_message(f"**🚀 BROADCAST STARTED JUST WAIT WE WILL SEND TO**  {interaction.guild.member_count} members...", ephemeral=True)
 
     sent = 0
     failed = 0
